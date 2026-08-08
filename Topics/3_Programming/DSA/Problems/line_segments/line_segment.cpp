dbd8ee1bd4dcf1fb18fcdba105892b9ec52130b4
konradkriehmig <konrad.kriehmig@insead.edu>
2026-08-08 23:11:32 +0100

    Checkpoint from VS Code for cloud agent session

--- Topics/3_Programming/DSA/Problems/line_segments/line_segment.cpp
+++ Topics/3_Programming/DSA/Problems/line_segments/line_segment.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <cmath>
 
-int main() {
-    std::cout << "hello world" << std::endl;
-    return 0;
+const double PI = 3.14159265358979323846;
+
+double get_angle(double x, double y) {
+    double angle = std::atan2(x,y);
+    return angle * (180.0 / PI);
 }
 
